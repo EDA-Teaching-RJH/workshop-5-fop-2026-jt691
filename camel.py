d834f369019a9e5b4c4592e2531656858b81e80d
jt691 <jt691@kent.ac.uk>
2026-02-02 14:48:34 +0000

    workshop 5

--- camel.py
+++ camel.py
@@ -1,1 +1,4 @@
-# Enter your code here
+#letters = ["A","B","C","D","E","D","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+
+#for letter in letters:
+ #   print(letter).lower
--- coffee.py
+++ coffee.py
@@ -1,1 +1,26 @@
-#Your code goes here
+due = 75
+accepted_coins = [50, 20, 10 ,5 ]    #states the coin value that the machine will accept
+
+while due > 0:   
+        coin = int(input("insert coins"))   
+
+
+        if coin in accepted_coins:
+            due -= coin                  # coin value must be within accepted_value , due -= coin is the same as due = due - coin(this is done to keep the value )
+            print ("Amount due:" , due )
+    
+        else:
+            print("Invalid coin entered, please enter", abs(due))       # abs makes the value positive
+    
+# else:
+
+
+print("Change owed:", abs(due))
+
+
+  
+    
+    
+    
+    
+
